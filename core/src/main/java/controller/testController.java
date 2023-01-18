2ce45f39c9c5c7f5eb43f16e3c133e0599a264c9
chulho.jeong <chulho.jeong@sktelecom.com>
2023-01-18 12:09:01 +0900

    테스트코드 추가

--- core/src/main/java/controller/testController.java
+++
@@ -1,7 +0,0 @@
-package controller;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class testController {
-}
---
+++ core/src/main/java/multi_module_test/TestService.java
@@ -0,0 +1,7 @@
+package multi_module_test;
+
+public class TestService {
+    public String test() {
+        return "Core 테스트";
+    }
+}
